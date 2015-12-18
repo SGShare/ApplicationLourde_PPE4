40700076ed0fceb214152e6822a9d793568912d3
Jamain Jordan <jordan.jamain@hotmail.fr>
2015-12-18 14:47:28 +0100

    base application c#
    Modele, Controleur Form principale et connection à la BDD

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApplicationLourde_PPE4
+{
+    public partial class FormInsertionBDD : Form
+    {
+        public FormInsertionBDD()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void importToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           controleur.init();
+            controleur.Vmodele.seconnecter();
+            if (controleur.Vmodele.Connopen == false)
+            {
+                MessageBox.Show("Erreur ouverture bdd : ", "PBS Connection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                controleur.Vmodele.import();
+                if (controleur.Vmodele.Chargement == true)
+                {
+                    
+                }
+            }
+            controleur.Vmodele.sedeconnecter();
+           
+           
+        }
+
+        private void FormInsertionBDD_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            controleur.Vmodele.sedeconnecter();
+        }
+    }
+}
---
+++ modele.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLourde_PPE4
+{
+    class modele
+    {
+        private MySqlConnection myConnection;
+        private bool connopen = false;
+        private bool errgrave = false;
+        private bool chargement = false;
+
+
+        #region accesseur
+        public MySqlConnection MyConnection
+        {
+            get { return myConnection; }
+            set { myConnection = value; }
+        }
+
+        public bool Connopen
+        {
+            get { return connopen; }
+            set { connopen = value; }
+        }
+
+        public bool Errgrave
+        {
+            get { return errgrave; }
+            set { errgrave = value; }
+        }
+
+        public bool Chargement
+        {
+            get { return chargement; }
+            set { chargement = value; }
+        }
+        #endregion
+
+
+        public void seconnecter()
+        {
+            string myConnectionString = "Database=bd_ppe4_lldj;Data Source=localhost;User Id=root;";
+            myConnection = new MySqlConnection(myConnectionString);
+            try
+            {
+                myConnection.Open();
+                connopen = true;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Erreur ouverture bdd : " + err, "PBS connection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connopen = false;
+                errgrave = true;
+            }
+        }
+
+        public void sedeconnecter()
+        {
+            if (!connopen)
+                return;
+            try
+            {
+                myConnection.Close();
+                myConnection.Dispose();
+                connopen = false;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Erreur fermeture bdd : " + err, "PBS deconnection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                errgrave = true;
+            }
+        }
+    }
+
+}
